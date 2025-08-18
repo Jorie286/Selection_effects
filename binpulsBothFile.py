868d8504a98f9974cdae44bd08a99b23fca08d6a
Jorie286 <mcdermott.286@osu.edu>
2025-08-18 15:27:40 -0500

    Modify the grouping of selection effects.

--- binpulsBothFile.py
+++ binpulsBothFile.py
@@ -57,8 +57,6 @@ pulsar_data_out["det1"] = None
 pulsar_data_out["det2"] = None
 pulsar_data_out["SNR1"] = None
 pulsar_data_out["SNR2"] = None
-pulsar_data_out["alt_det1"] = None
-pulsar_data_out["alt_det2"] = None
 
 # iterate through each row of the simulated pulsar data and determine if the pulsar is detectable
 for i, row in pulsar_data_out.iterrows():
@@ -68,9 +66,12 @@ for i, row in pulsar_data_out.iterrows():
         # Units: none, M_sun, M_sun, none, s, s, s^2, s^2, T, T, Kpc, Kpc, Kpc, Kpc, Kpc, Kpc, km/s, km/s, km/s, km/s, km/s, km/s, mJy Kpc^2, mJy Kpc^2
         ID, M_1, M_2, P_orb, e, a, P_1, P_2, P_dot1, P_dot2, B_1, B_2, x, y, z, vx, vy, vz, L1, L2, type1, type2 = row['ID'], row['m1(Msun)'], row['m2(Msun)'], row['porb(days)'], row['e'], row['a(AU)'], row['p1(s)'], row['p2(s)'], row['pdot1(s/s)'], row['pdot2(s/s)'], row['b1(T)'], row['b2(T)'], row['x(kpc)'], row['y(kpc)'], row['z(kpc)'], row['vx(km/s)'], row['vy(km/s)'], row['vz(km/s)'], row['l1(mJy kpc²)'], row['l2(mJy kpc²)'], row['type1'], row['type2']
 
+        # get the Bagchi correction of the objects in the system
+        det1b, det2b = selection_effects.DNS_NSBH_sel_eff(P_1, P_2, P_orb, e, type1, type2)
+
         # check to make sure that the pulsar is not below the death lines, if it is spin period will be None
-        det1 = selection_effects.death_lines(P_1, P_dot1, M_1, x, y, z, L1)
-        det2 = selection_effects.death_lines(P_2, P_dot2, M_2, x, y, z, L2)
+        det1d = selection_effects.death_lines(P_1, P_dot1, M_1, x, y, z, L1)
+        det2d = selection_effects.death_lines(P_2, P_dot2, M_2, x, y, z, L2)
 
         # check to see if each object is a neutron star and if it is radio detectable, if it is not, save values as None, else compute the values
         if pd.isna(P_1) == True or det1==0:
@@ -89,16 +90,15 @@ for i, row in pulsar_data_out.iterrows():
 
             f_b2 = selection_effects.f_beaming(P_2)
 
-        # get the Bagchi correction of the objects in the system
-        alt_det1, alt_det2 = selection_effects.DNS_NSBH_sel_eff(P_1, P_2, P_orb, e, type1, type2)
-
     else:
         # define each constant in the pulsar data as what it is for greater readability
         # Units: none, M_sun, none, s, s^2, T, Kpc, Kpc, Kpc, km/s, km/s, km/s, mJy Kpc^2
         ID, M_1, P_orb, e, a, P_1, P_dot1, B_1, x, y, z, vx, vy, vz, L1 = row['ID'], row['m1(Msun)'], row['porb(days)'], row['e'], row['a'], row['p1(s)'], row['pdot1(s/s)'], row['b1(T)'], row['x(kpc)'], row['y(kpc)'], row['z(kpc)'], row['vx(km/s)'], row['vy(km/s)'], row['vz(km/s)'], row['l1(mJy kpc²)']
 
+        det1b = 1 # Bagchi correction always 1 for single
+
         # check to make sure that the pulsar is not below the death lines, if it is spin period will be None
-        det1 = selection_effects.death_lines(P_1, P_dot1, x, y, z, L1)
+        det1d = selection_effects.death_lines(P_1, P_dot1, x, y, z, L1)
 
         # check to see if each object is a neutron star, if it is not, save values as None, else compute the values
         if pd.isna(P_1) == True or det1==0:
@@ -115,18 +115,24 @@ for i, row in pulsar_data_out.iterrows():
     # get the galacitc coordinates of the object
     l, b, d = gal_cart.cart2gal(x, y, z, degree=True)
     if s[-1](l, b) == 1 and pd.isna(S_min1)==False and pd.isna(S_min1)==False and pd.isna(area)==False and pd.isna(T_sky)==False and pd.isna(f_b1)==False and pd.isna(f_b2)==False and pd.isna(SNR1)==False and pd.isna(SNR2)==False: # check to see if the pulsar is within the survey's viewing area. If it is, save the info.
+
+        # get radiometer equaiton detection
+        det1r = (L1 >= S_min1*area).astype(int)
+        det2r = (L2 >= S_min2*area).astype(int)
+
+        det1 = np.minimum.reduce([det1b, det1r, det1d])
+        det2 = np.minimum.reduce([det2b, det2r, det2d])
+
         pulsar_data_out.loc[i, "S_min1*area"] = S_min1*area
         pulsar_data_out.loc[i, "S_min2*area"] = S_min2*area
         pulsar_data_out.loc[i, "Area"] = area
         pulsar_data_out.loc[i, "T_sky"] = T_sky
         pulsar_data_out.loc[i, "f_beaming1"] = f_b1
         pulsar_data_out.loc[i, "f_beaming2"] = f_b2
-        pulsar_data_out.loc[i, "det1"] = (L1 >= S_min1*area).astype(int)
-        pulsar_data_out.loc[i, "det2"] = (L2 >= S_min2*area).astype(int)
+        pulsar_data_out.loc[i, "det1"] = det1
+        pulsar_data_out.loc[i, "det2"] = det2
         pulsar_data_out.loc[i, "SNR1"] = SNR1
         pulsar_data_out.loc[i, "SNR2"] = SNR2
-        pulsar_data_out.loc[i, "alt_det1"] = alt_det1
-        pulsar_data_out.loc[i, "alt_det2"] = alt_det2
 
 # save the updated pulsar data to two new csv files
 pulsar_data_out.to_csv(str(output_name), index=False, sep=";")
